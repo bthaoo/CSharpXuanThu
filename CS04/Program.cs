fbb26d9df3483facc7d33f0ce5566caa5d55449e
Bthao <ntbichthao2002@gmail.com>
2023-10-08 00:37:20 +0700

    C4

--- CS04/Program.cs
+++ CS04/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+{
+    static void Main(string[] args)
+    {
+        bool kq; 
+        // toan tu so sanh == > < >= <= !=
+        int a = 5;
+        int b = 6;
+
+        kq = a == b;
+        Console.WriteLine("a == b ==> {0}", kqua); /*true/false duoc dien vao {0}*/
+        Console.WriteLine($"a != b ==> {a != b}");
+        Console.WriteLine($"a > b ==> {a > b}");
+        Console.WriteLine($"a >= b ==> {a >= b}");
+        Console.WriteLine($"a < b ==> {a < b}");
+        Console.WriteLine($"a <= b ==> {a <= b}");
+
+        // && : va
+        // || : hoac
+        // ! : phu dinh
+        bool c = true;
+        bool d = false;
+        kq = c || d;
+        Console.WriteLine(kq);
+    }
+}
