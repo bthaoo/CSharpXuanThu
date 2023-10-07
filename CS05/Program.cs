6b3410795f9df345bf7f3527c2e6b86160485d7b
Bthao <ntbichthao2002@gmail.com>
2023-10-08 01:47:48 +0700

    CS56

---
+++ CS05/Program.cs
@@ -0,0 +1,110 @@
+﻿﻿using System;
+namespace CS05
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             if (dieu_kien_logic)
+                dong_lenh;
+
+            if (dieu_kien_logic)
+            {
+                ... (khoi lenh if)
+            }
+
+            else if (dieu_kien_1)
+            {
+              ... (khoi lenh else)
+            }
+            else if (dieu_kien_2)
+            {
+              ... (khoi lenh else)
+            }
+            else 
+            {
+
+            }
+
+            */
+ 
+            int a;
+            Console.WriteLine("Nhap so nguyen a: ");
+            a = int.Parse(Console.ReadLine());
+
+            //kiem tra a co phai so chan k
+            //a % 2 == 0
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Ket qua: ");
+                Console.WriteLine($"So {a} la so chan");
+            }
+
+            else
+            {
+                Console.WriteLine("Ket qua: ");
+                Console.WriteLine($"So {a} la so le");
+            }
+            Console.WriteLine("The end");
+
+
+
+
+            //TINH DIEM TRUNG BINH
+            //[8 - 10] : gioi
+            //[6.5 - 8] : kha
+            //[5 - 6.5] : trung binh
+            //[0 - 5] : yeu
+
+            float dtb;
+            Console.WriteLine("Hay nhap diem trung binh: ");
+            dtb = float.Parse(Console.ReadLine());
+            if (dtb < 5.0)
+            {
+                Console.WriteLine("Hoc luc Yeu");
+            }    
+            else if (dtb < 6.5)
+            {
+                Console.WriteLine("Hoc luc Trung binh");
+            }
+            else if (dtb < 8)
+            {
+                Console.WriteLine("Hoc luc Kha");
+            }
+            else if (dtb <= 10)
+            {
+                Console.WriteLine("Hoc luc Gioi");
+            }
+            else
+            {
+                Console.WriteLine("Nhap sai so diem");
+            }
+
+
+
+
+            //TIM SO LON NHAT
+            float b, c;
+            Console.WriteLine("So b: ");
+            b = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("So c: ");
+            c = float.Parse(Console.ReadLine());
+            float max;
+            
+            if (b > c)
+                max = b;
+            else
+                max = c;
+            Console.WriteLine("So lon nhat la: {0}", max);
+
+
+            //Cach khac (toan tu 3 ngoi): (dieu_kien) ? bieuthuc1 : bieu thuc2;
+            //float b, c;
+            //float max;
+            //max = (b > c) ? b : c;
+            //Console.WriteLine(max);
+        }
+    }
+}
---
+++ CS06/Program.cs
@@ -0,0 +1,81 @@
+﻿﻿using System;
+namespace CS06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = 2;
+            //if (a == 1)
+            //{
+            //    Console.WriteLine("Gia tri a bang mot");
+            //}
+            //else if (a == 2)
+            //{
+            //    Console.WriteLine("Gia tri a bang hai");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hay kiem tra so khac");
+            //}
+
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Gia tri a bang mot");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Gia tri a bang hai");
+                    break ;
+                default: // optional co hay ko deu duoc
+                    Console.WriteLine("Hay thu so khac");
+                    break;
+            }
+
+        //menu tinh 2 so
+            int b, c;
+            Console.WriteLine("Nhap so b: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap so c: ");
+            c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hay chon lenh");
+            Console.WriteLine("1. Tinh tong");
+            Console.WriteLine("2. Tinh hieu");
+            Console.WriteLine("3. Tinh tich");
+            Console.WriteLine("4. Tinh thuong");
+
+            char d;
+            L1:
+            d = Console.ReadKey().KeyChar; //readkey: nhap tu ban phim
+            Console.WriteLine();
+                
+            switch (d)
+            {
+                case '1':
+                    Console.WriteLine($"Tong la: {b + c}");
+                break;
+
+                case '2':
+                    Console.WriteLine($"Hieu la: {b - c}");
+                break;
+
+                case '3':
+                    Console.WriteLine($"Tich la: {b * c}");
+                break;
+
+                case '4':
+                    Console.WriteLine($"Thuong la: {b / c}");
+                break;
+
+                default:
+                    Console.WriteLine("Hay chon lenh khac");
+                    goto L1; //điều hướng đến dòng lệnh mình muốn
+                break;
+            }
+        }
+    }
+}
